ecb62fe43d9ba95cd02ba0e25c46ebf0656804e3
cabdikarim <cabdikarim@gmail.com>
2024-11-19 09:19:29 +0000

    first commit

---
+++ src/scribt.js
@@ -0,0 +1,9 @@
+let result = document.querySelector(".result");
+function Tasbiix(){
+    result.textContent = parseInt(result.textContent)+1
+    return
+}
+
+function reset(){
+    result.textContent=0
+}
